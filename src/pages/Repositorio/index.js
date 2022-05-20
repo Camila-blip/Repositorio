db487600e29a034f45b0c146ec07d5d76149e6d6
Camila <millahrodriguez@outlook.com>
2022-05-20 18:03:53 -0300

    criação da tela de detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import './styles.scss';
+import {Container, Owner, Loading, BackButton} from './styles';
+import api from '../../services/api';
+import {FaArrowLeft} from 'react-icons/fa';
+
 
 export default function Repositorio(){
     const { repositorio } = useParams(); 
-    return(
-        <>
-            <h1>
-                {repositorio}
-            </h1>
-        </>
-    )
+    const [repositorioNome, setRepositorioNome] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(()=>{
+        async function load(){
+            alert(repositorio)
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${repositorio}`),
+                api.get(`/repos/${repositorio}/issues`,{
+                    params:{
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+            console.log(repositorioData.data)
+            setRepositorioNome(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    },[repositorio]);
+
+    if(loading){
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
+    
+        return(
+            <Container>
+                <BackButton>
+
+                </BackButton>
+
+                <Owner>
+                    <img src={repositorioNome.owner.avatar_url} alt={repositorioNome.owner.login} />
+                    <h1>{repositorioNome.name}</h1>
+                    <p>{repositorioNome.description}</p>
+                </Owner>
+            </Container>
+        )
+    
 }
---
+++ src/pages/Repositorio/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 700px;
+    background: #fff;
+    border-radius: 4px;
+    box-shadow: 0 0 20px rgba(0 , 0, 0, 0.2);
+    padding: 0 30px;
+    margin: 80px auto;
+`;
+
+export const Owner = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img{
+        max-width: 150px;
+        border-radius: 20%;
+        margin: 20px 0;
+    }
+    h1{
+        font-size: 30px;
+        color: #0d2636;
+    }
+    p{
+        margin-top: 5px;
+        font-size: 14px;
+        color: #000;
+        text-align: center;
+        line-height: 1.4;
+        max-width: 400px;
+    }
+`;
+
+export const Loading = styled.div`
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+export const BackButton = styled.div``;
+
